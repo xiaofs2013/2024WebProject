0ec944ceb91273e3a9a19b65a4751c04dda5f5a3
O-XIAWOENXUAN <469449388@qq.com>
2024-09-13 17:02:50 +0800

    init

---
+++ work/116_Module的语法/hello.js
@@ -0,0 +1,9 @@
+// export var hello = "hello";  //导出hello变量
+
+//一个文件中只能有一个export default
+export default function (){
+    console.log("hahaa")
+}
+function gameName(name) { //定义gameName函数
+    console.log()
+}
---
+++ work/116_Module的语法/index.js
@@ -0,0 +1,6 @@
+// import {hello} from "./hello.js";
+import * as myHello from "./hello.js"; //引入hello.js文件 用myHello别名代替
+
+
+myHello.gameName();
+
